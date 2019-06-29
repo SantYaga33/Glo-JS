8eebe986bc37fdd2579e2083a054ec9194c5232d
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-06-29 14:50:42 +0400

    доп ДЗ 2-1

---
+++ js/lesson2.js
@@ -0,0 +1,18 @@
+let num = 266219, result = 1; 
+
+let numbers = num.toString().split(''); 
+for (let i = 0; i <= numbers.length-1; i++) {
+  result *= Number(numbers[i]);
+}
+
+console.log(`Произведение цифр числа ${num} равно :`, result);
+
+let degreeNum = 3;
+let degree = result ** 3;
+console.log(`Возведение числа ${result} в степень ${degreeNum} равно :`, degree);
+
+let degreeNumbers = degree.toString().split('');
+console.log(`Первые два символа ${degree} будут равны :`, degreeNumbers[0] + degreeNumbers[1]);
+
+
+
--- js/main.js
+++
@@ -1,4 +0,0 @@
-let money, income, addExpenses, deposit, mission, period;
-
-alert('Ну что ж - погнали !!!');
-console.log('Любой тест ))');
