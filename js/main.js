cbf418c4871e0a899a40aa3983236890026e4e81
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-06-29 12:57:29 +0400

    урок 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-let money, income, addExpenses, deposit, mission, period;
+let money        = 12000,
+    income       = 'фриланс',
+    addExpenses  = 'аренда, поездки,  няня, садик',
+    deposit      = true,
+    mission      = 100000,
+    period       = 12;
 
-alert('Ну что ж - погнали !!!');
-console.log('Любой тест ))');
+    
+console.log('Тип данных money: ', typeof money); 
+console.log('Тип данных income: ', typeof income); 
+console.log('Тип данных deposit: ', typeof deposit);
+console.log(`Количество символов в слове ${income} = `, income.length); 
+console.log(`В течении ${period} месяцев - цель заработать ${mission} долларов`); 
+
+addExpenses.toLowerCase();
+console.log( addExpenses.split(', ') );
+
+let budgetDay = 12000 / 30;
+console.log(`budgetDay = ${budgetDay}`);
+console.log(`Остаток от деления = ${budgetDay % 30}`);
+
