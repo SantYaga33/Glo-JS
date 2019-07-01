297f77a26bb07434a9b91c9f1102744600d0ac7e
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-07-01 18:26:07 +0400

    3 урок 1я часть

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-let money, income, addExpenses, deposit, mission, period;
+let lang, daysWeek, ruWeek = [], enWeek = [];
+ruWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+enWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-alert('Ну что ж - погнали !!!');
-console.log('Любой тест ))');
+lang = prompt('Введите значение переменной lang', 'В виде ru или en');
+// вариант 1
+if (lang == 'ru') {
+  daysWeek = ruWeek;
+} else {
+  daysWeek = enWeek;
+}
+console.log('Проверка :', daysWeek);
+// вариант 2
+switch (lang) {
+  case 'ru':
+    daysWeek = ruWeek;
+    break;
+  case 'en':
+    daysWeek = enWeek;
+    break;
+  default:
+    console.log('Введите значение переменной "lang"');
+    break;
+}
+console.log('Проверка :', daysWeek);
+// вариант 3
+let langWeek = [ruWeek, enWeek];
+daysWeek = lang === 'ru' ? langWeek[0] : langWeek[1];
+console.log('Проверка :', daysWeek);
+
