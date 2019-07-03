46e535bfcc6cbecdd2db22968c525d232d542831
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-07-04 02:52:23 +0400

    урок -5

--- js/main.js
+++ js/main.js
@@ -1,65 +1,72 @@
 'use strict';
 
-let money = 12000,
-  income = 'фриланс',
-  addExpenses = 'аренда, поездки,  няня, садик',
-  deposit = true,
-  mission = 100000,
-  period = 12;
+// == урок 5 ==
+let deposit = true,
+    mission = 100000,
+    money,
+    period,
+    budgetMonth,
+    budgetDay,
+    sumCharges;
+
+let start = function() {
+  do  {
+    money = prompt('Ваш месячный доход?');
+  } while (isNaN(money) || money == '' || money == null); 
+};
+start();
+
+let numberRequests = 2,
+    expenses = [],
+    howMoney = [],
+    howMoneyAnsw,
+    expensesAnsw;
+
+let getExpensesMonth = function () {
+  for (let i = 0; i < numberRequests; i++) {
+    do {
+      expensesAnsw = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+    } while (!isNaN(expensesAnsw) || expensesAnsw == '' || expensesAnsw == null); 
+    expenses.push(expensesAnsw);
+
+    do {
+      howMoneyAnsw = prompt('Во сколько это обойдется?', 'Укажите сумму в цифрах');
+    } while (isNaN(howMoneyAnsw) || howMoneyAnsw == '' || howMoneyAnsw == null); 
+    howMoney.push(+howMoneyAnsw);
+  }
+
+  sumCharges = howMoney.reduce(function (sum, current) {
+    return sum + current;
+  });
+  return sumCharges;
+};
+getExpensesMonth();
+ 
+// == промежуточная проверка ==
+// console.log('howMoney: ', howMoney);
+// console.log('expenses: ', expenses);
+// console.log('Сумма всех расходов в месяц: ', sumCharges);
+
+let getAccumulatedMonth = function () {
+  let resalt = money - sumCharges;
+  return resalt;
+};
+
+let accumulatedMonth = getAccumulatedMonth();
+// console.log('accumulatedMonth: ', accumulatedMonth);
 
 
-// console.log('Тип данных money: ', typeof money); 
-// console.log('Тип данных income: ', typeof income); 
-// console.log('Тип данных deposit: ', typeof deposit);
-// console.log(`Количество символов в слове ${income} = `, income.length); 
-// console.log(`В течении ${period} месяцев - цель заработать ${mission} долларов`); 
-
-addExpenses.toLowerCase();
-// console.log( addExpenses.split(', ') );
-
-let budgetDay = 12000 / 30;
-// console.log(`budgetDay = ${budgetDay}`);
-// console.log(`Остаток от деления = ${budgetDay % 30}`);
-
-// == продолжение урока вводим новые данные == 
-alert('Вводим новые данные');
-money       = prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-
-// console.log('addExpenses: ', addExpenses.split(','));
-
-deposit = confirm('Есть ли у вас депозит в банке?\nЕсли "ДА" - нажмите "OK"\nЕсли "НЕТ" - "Отмена"');
-
-// console.log('Тип данных money: ', typeof money);
-// console.log('Тип данных income: ', typeof income);
-// console.log('Тип данных deposit: ', typeof deposit);
-
-let numberRequests = 2, expenses = [], howMoney = [];
-for (let i = 0; i < numberRequests; i++) {
-  expenses.push(prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Напишите прописью'));
-  howMoney.push(+prompt('Во сколько это обойдется?', 'Укажите сумму в цифрах'));
-}
-// console.log('Ежемесячные расходы : ', expenses);
-// console.log('Ежемесячные расходы в цифрах: ', howMoney);
-
-let budgetMonth, sumCharges;
-sumCharges = howMoney.reduce(function (sum, current) {
-  return sum + current;
-});
+let getTargetMonth = function () {
+  period = Math.ceil(mission / accumulatedMonth);
+  if (period <= 0) {
+    return console.log('Цель не будет достигнута');
+  }
+  return period;
+};
 
 budgetMonth = money - sumCharges;
-// console.log('Доход за месяц: ', budgetMonth);
-
-period = Math.ceil(mission / budgetMonth);
-// console.log(`Мы достигнем цели ${mission}долл. через: ${period} месяцев`);
-
 budgetDay = Math.floor(budgetMonth / 30);
-// console.log(`Наш ежеднейный бюджет равен: ${budgetDay}`);
 
-
-
-// == урок 4 ==
-console.log('4-lesson - код выше не удален ( закоментирован )-может понадобиться');
 let getStatusIncome  = function() { 
   if (budgetDay >= 800) {
     return ('Высокий уровень дохода');
@@ -73,37 +80,4 @@ let getStatusIncome  = function() {
 };
 
 console.log( 'Наш уровень дохода :', getStatusIncome() );
-
-let showTypeof = function (param) {
-  console.log(param, typeof param);
-};
-showTypeof(money);
-showTypeof(income);
-showTypeof(deposit);
-
-
-
-let getExpensesMonth = function () {
-  sumCharges = howMoney.reduce(function (sum, current) {
-    return sum + current;
-  });
-  return sumCharges;
-};
-getExpensesMonth();
-// console.log( 'Сумма всех расходов в месяц: ', getExpensesMonth() );
-
-let getAccumulatedMonth = function () {
-  let resalt = money - sumCharges;
-  return resalt;
-};
-let accumulatedMonth = getAccumulatedMonth();
-console.log('Чистый доход за 12 месяцев составит: ', accumulatedMonth * 12 );
-// console.log( 'Чистый доход в месяц: ', getAccumulatedMonth() );
-
-let getTargetMonth = function () {
-  period = Math.ceil(mission / accumulatedMonth);
-  return period;
-};
-
-console.log(`Достигнем цели через ${getTargetMonth()} месяц(ев)`);
 
