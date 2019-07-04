61c0e9792c7d5ae83b9444a088811fcf476b8cd9
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-07-04 04:22:52 +0400

    lesson 5_1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-let money, income, addExpenses, deposit, mission, period;
+'use strict';
 
-alert('Ну что ж - погнали !!!');
-console.log('Любой тест ))');
+let arr = ['234234', '5456456', '5345345', '464565', '243234', '45566', '40000'];
+
+arr.forEach( function(e) {
+  if ( e.startsWith('2') || e.startsWith('4') ) {
+    console.log(e);
+  }
+});
+
+
+let isPrime = function(number) {
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+         return false;
+      }
+      return true;
+    }
+};
+
+let getPrimeNumber = function(maxNumber) {
+  for (let i = 2; i < maxNumber; i++) {
+    if ( isPrime(i) ){
+      console.log(`Делители числа ${i}: 1 и ${i}`);
+    }
+  }
+};
+getPrimeNumber(100);
