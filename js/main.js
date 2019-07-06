ebbe68e25a3fe3c3b67b29aef5d6f291231c03ff
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-07-06 15:16:25 +0400

    lesson-6_1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-let money, income, addExpenses, deposit, mission, period;
 
-alert('Ну что ж - погнали !!!');
-console.log('Любой тест ))');
+let week = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+    day = new Date(),
+    currentDay = day.getDay(),
+    dayOfWeek = document.querySelector('.dayOfWeek');
+
+let getDayOfWeek = function() {
+  for (let i = 0; i < week.length; i++ ) {
+    let div = document.createElement('div');
+    if (week[i] == week[currentDay]) {
+      div.innerHTML = `<b>${week[currentDay]}</b><br>`;
+      dayOfWeek.appendChild(div);
+    } else if (week[i] == week[0] || week[i] == week[6]) {
+      div.innerHTML = `<i>${week[i]}</i><br>`;
+      dayOfWeek.appendChild(div);
+    } else {
+      div.innerHTML = `${week[i]}<br>`;
+      dayOfWeek.appendChild(div);
+    }
+  }
+};
+getDayOfWeek();
