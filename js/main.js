a90fa23c4e3d0589642e24c1d9117ce070555a57
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-07-06 22:29:53 +0400

    ++

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ let week = ['воскресенье', 'понедельник', 'вторник'
     day = new Date(),
     currentDay = day.getDay(),
     dayOfWeek = document.querySelector('.dayOfWeek');
+   
 
 let getDayOfWeek = function() {
   for (let i = 0; i < week.length; i++ ) {
     let div = document.createElement('div');
 
     if (week[i] == week[currentDay] && week[i] == week[0] || week[i] == week[6]) {
-      div.innerHTML = `<b><i>${week[currentDay]}</i></b><br>`;
+      div.innerHTML = `<b><i>${week[i]}</i></b><br>`;
       dayOfWeek.appendChild(div);
     } else if (week[i] == week[currentDay]) {
-      div.innerHTML = `<b>${week[currentDay]}</b><br>`;
+      div.innerHTML = `<b>${week[i]}</b><br>`;
       dayOfWeek.appendChild(div);
     } else if (week[i] == week[0] || week[i] == week[6]) {
       div.innerHTML = `<i>${week[i]}</i><br>`;
@@ -24,3 +25,5 @@ let getDayOfWeek = function() {
   }
 };
 getDayOfWeek();
+
+
