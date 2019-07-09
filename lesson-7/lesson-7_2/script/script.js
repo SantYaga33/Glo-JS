71d2ecfe091bd78b851161dd661b6077accee5de
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-07-10 02:16:46 +0400

    ++

---
+++ lesson-7/lesson-7_2/script/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let startElement              = document.querySelector('#start'),
+    incomePluseElement        = document.querySelector('.income > button'),
+    expensesPluseElement      = document.querySelector('.expenses > button'),
+    checkBoxElement           = document.querySelector('#deposit-check'),
+    additionalItemElement     = document.querySelectorAll('.additional_income-item'),
+    budgetDayElement          = document.querySelector('.budget_day-value'),
+    budgetMonthElement        = document.querySelector('.budget_month-value'),
+    expensesMonthElement      = document.querySelector('.expenses_month-value'),
+    accumulatedMonthElement   = document.querySelector('.accumulated_month-value'),
+    additionalIncomeElement   = document.querySelector('.additional_income-value'),
+    additionalExpensesElement = document.querySelector('.additional_expenses-value'),
+    incomePeriodElement       = document.querySelector('.income_period-value'),
+    targetMonthElement        = document.querySelector('.target_month-value'),
+    salaryAmountElement       = document.querySelector('.salary-amount'),
+    incomeTitleElement        = document.querySelector('.income-title'),
+    incomeAmountElement       = document.querySelector('.income-amount'),
+    expensesTitleElement      = document.querySelector('.expenses-title'),
+    expensesAmountElement     = document.querySelector('.expenses-amount'),
+    additionalExpItemElement  = document.querySelector('.additional_expenses-item'),
+    targetAmountElement       = document.querySelector('.target-amount'),
+    periodSelectElement       = document.querySelector('.period-select');
+
+    console.log(startElement, incomePluseElement, expensesPluseElement, checkBoxElement, additionalItemElement, budgetDayElement, budgetMonthElement, expensesMonthElement );
+    console.log(accumulatedMonthElement, additionalIncomeElement, additionalExpensesElement, incomePeriodElement, targetMonthElement, salaryAmountElement);
+    console.log(incomeTitleElement, incomeAmountElement, expensesTitleElement, expensesAmountElement, targetAmountElement, periodSelectElement, additionalExpItemElement);
