c11abbe3acdb512b26fd6bfddb1272ae01e2da84
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-07-25 03:53:17 +0400

    слайдер  на 99% - проблема с перезапуском

--- lesson-12/js/main.js
+++ lesson-12/js/main.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function() {
     };
     updateClock();
   };
-  countTimer('24 july 2019');
+  countTimer('28 july 2019');
 
 // Меню
   const toggleMenu = () => {
@@ -59,20 +59,17 @@ window.addEventListener('DOMContentLoaded', function() {
   toggleMenu();
 
 // плавная прокрутка до якоря 
-const anchors = document.querySelectorAll('a[href*="#"]');
-for (let anchor of anchors) {
-  anchor.addEventListener('click', (e) => {
-    e.preventDefault();
-    const ourId = anchor.getAttribute('href');
-     
-      console.log('  ourId: ',   ourId);
-      console.log(document.querySelector('' + ourId));
-    document.querySelector('' + ourId).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-  });
-}
+// const anchors = document.querySelectorAll('a[href*="#"], a[href*="#"]:not(.prev, .next)');
+// for (let anchor of anchors) {
+//   anchor.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     const ourId = anchor.getAttribute('href');
+//     document.querySelector('' + ourId).scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start'
+//     });
+//   });
+// };
 
 // popup
   const togglePopUp = () => {
@@ -148,5 +145,92 @@ for (let anchor of anchors) {
 
   };
   tab();
+
+//  слайдер 
+  const slider = () => {
+    const slideElem = document.querySelectorAll('.portfolio-item'),
+          buttonElem = document.querySelectorAll('.portfolio-btn'),
+          dotElem = document.querySelectorAll('.dot'),
+          sliderElem = document.querySelector('.portfolio-content');
+
+    let currentSlide = 0, interval;
+
+    const prevSlide = (elem, index, strClass) => {
+      elem[index].classList.remove(strClass);
+    };
+
+    const nextSlide = (elem, index, strClass) => {
+      elem[index].classList.add(strClass);
+    };
+
+    const autoPlaySlide = () => {
+      prevSlide(slideElem, currentSlide, 'portfolio-item-active');
+      prevSlide(dotElem, currentSlide, 'dot-active');
+      currentSlide++;
+      if (currentSlide >= slideElem.length) {
+        currentSlide = 0;
+      }
+       nextSlide(slideElem, currentSlide, 'portfolio-item-active');
+       nextSlide(dotElem, currentSlide, 'dot-active');
+    };
+
+    const startSlide = (time = 2000) => {
+      interval = setInterval(autoPlaySlide, time);
+    };
+
+    const stopSlide = () => {
+      clearInterval(interval);
+    };  
+
+    sliderElem.addEventListener('click', (event) => {
+      event.preventDefault();
+      let target = event.target;
+      if (!target.matches('.portfolio-btn, .dot')) {
+        return;
+      }
+      prevSlide(slideElem, currentSlide, 'portfolio-item-active');
+      prevSlide(dotElem, currentSlide, 'dot-active');
+
+      if (target.matches('#arrow-right')) {
+        currentSlide++;
+      } else if (target.matches('#arrow-left')) {
+        currentSlide--;
+      } else if (target.matches('.dot')) {
+        dotElem.forEach((elem, index) => {
+          if (elem === target) {
+            currentSlide = index;
+          }
+        });
+      }
+      if (currentSlide >= slideElem.length) {
+        currentSlide = 0;
+      }
+      if (currentSlide < 0) {
+        currentSlide = slideElem.length - 1;
+      }
+      nextSlide(slideElem, currentSlide, 'portfolio-item-active');
+      nextSlide(dotElem, currentSlide, 'dot-active');
+    });
+
+    sliderElem.addEventListener('mouseover', (event) => {
+      if (event.target.matches('.portfolio-btn') || 
+          event.target.matches('.dot')) {
+        stopSlide();
+      }
+    });
+
+    sliderElem.addEventListener('mouseout', (event) => {
+      if (event.target.matches('.portfolio-btn') ||
+        event.target.matches('.dot')) {
+        startSlide(2000);
+      }
+    });
+
+    startSlide(2000);
+
+
+  };
+  slider();
+
 });
 
